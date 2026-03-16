2ac3ce9478dbfbaf75bb6b5fe9b9726ba05be33c
Hyunwoo Gu <realization0327@gmail.com>
2026-03-16 11:30:15 +0000

    feat(payment-api): 결제 API 엔드포인트 추가

---
+++ domains/payment/payment-api/src/main/kotlin/devcoop/occount/payment/api/PaymentApiApplication.kt
@@ -0,0 +1,25 @@
+package devcoop.occount.payment.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@EntityScan(basePackages = ["devcoop.occount.db"])
+@EnableJpaRepositories(basePackages = ["devcoop.occount.db"])
+@SpringBootApplication(
+    exclude = [UserDetailsServiceAutoConfiguration::class],
+    scanBasePackages = [
+        "devcoop.occount.payment",
+        "devcoop.occount.db",
+        "devcoop.occount.kafka",
+    ],
+)
+class PaymentApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentApiApplication>(*args)
+}
