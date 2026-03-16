5e31cac97c4df97efa78e3d44d05fcd1c76ec3b9
Hyunwoo Gu <realization0327@gmail.com>
2026-03-16 11:29:24 +0000

    feat(order-api): 주문 API 엔드포인트 추가

---
+++ domains/order/order-api/src/main/kotlin/devcoop/occount/order/api/OrderApiApplication.kt
@@ -0,0 +1,25 @@
+package devcoop.occount.order.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@EntityScan(basePackages = ["devcoop.occount.db"])
+@EnableJpaRepositories(basePackages = ["devcoop.occount.db"])
+@SpringBootApplication(
+    exclude = [UserDetailsServiceAutoConfiguration::class],
+    scanBasePackages = [
+        "devcoop.occount.order",
+        "devcoop.occount.db",
+        "devcoop.occount.kafka",
+    ],
+)
+class OrderApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderApiApplication>(*args)
+}
