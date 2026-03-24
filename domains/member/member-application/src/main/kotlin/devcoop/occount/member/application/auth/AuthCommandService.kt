99b7140cb8f7cb312c00125a035645b63425ad43
Hyunwoo Gu <realization0327@gmail.com>
2026-03-24 20:39:47 +0900

    refactor: user 중복 쿼리 확인 로직 삭제
    기존 중복 유저 확인 기능을 insert에게 위임 함으로 쿼리를 2개에서 1개로 축소 및 try 로직을 통한 안정성 증가

--- domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/auth/AuthCommandService.kt
+++ domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/auth/AuthCommandService.kt
@@ -6,6 +6,7 @@ import devcoop.occount.member.application.user.UserNotFoundException
 import devcoop.occount.member.application.user.UserRepository
 import devcoop.occount.member.domain.user.User
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -25,27 +26,27 @@ class AuthCommandService(
 
     @Transactional
     fun register(request: MemberRegisterRequest) {
-        if (userRepository.existsByUserEmail(request.userEmail)) {
+        val user = try {
+            userRepository.save(
+                User(
+                    userCiNumber = request.userCiNumber,
+                    username = request.userName,
+                    phone = request.userPhone,
+                    userEmail = request.userEmail,
+                    encodedPassword = passwordEncoder.encode(request.password),
+                    encodedPin = passwordEncoder.encode(defaultPin),
+                )
+            )
+        } catch (_: DataIntegrityViolationException) {
             throw UserAlreadyExistsException()
         }
 
-        val user = User(
-            userCiNumber = request.userCiNumber,
-            username = request.userName,
-            phone = request.userPhone,
-            userEmail = request.userEmail,
-            encodedPassword = passwordEncoder.encode(request.password),
-            encodedPin = passwordEncoder.encode(defaultPin),
-        )
-        val savedUser = userRepository.save(user)
-
         eventPublisher.publish(
             topic = DomainTopics.MEMBER_REGISTERED,
-            key = savedUser.getId().toString(),
+            key = user.getId().toString(),
             eventType = MEMBER_REGISTERED_EVENT_TYPE,
             payload = mapOf(
-                "userId" to savedUser.getId(),
-                "email" to request.userEmail,
+                "userId" to user.getId(),
             ),
         )
     }
