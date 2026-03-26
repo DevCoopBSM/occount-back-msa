8e929e76be60bdce2ed61e543afaf8c880600211
Hyunwoo Gu <realization0327@gmail.com>
2026-03-26 19:51:24 +0900

    refactor: order 메서드 userId 인자와 ORDER_REQUESTED_EVENT_TYPE payload에 userId 필드 추가

--- domains/order/order-application/src/main/kotlin/devcoop/occount/order/application/order/OrderService.kt
+++ domains/order/order-application/src/main/kotlin/devcoop/occount/order/application/order/OrderService.kt
@@ -4,7 +4,7 @@ import devcoop.occount.core.common.event.DomainTopics
 import devcoop.occount.core.common.event.EventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
+import java.util.*
 
 @Service
 class OrderService(
@@ -15,13 +15,14 @@ class OrderService(
     }
 
     @Transactional
-    fun order(request: OrderRequest) {
+    fun order(request: OrderRequest, userId: Long) {
         eventPublisher.publish(
             topic = DomainTopics.ORDER_REQUESTED,
             key = UUID.randomUUID().toString(),
             eventType = ORDER_REQUESTED_EVENT_TYPE,
             payload = mapOf(
                 "orderInfos" to request.orderInfos.map { orderInfo ->
+                    "userId" to userId
                     mapOf(
                         "itemId" to orderInfo.itemId,
                         "quantity" to orderInfo.orderQuantity,
