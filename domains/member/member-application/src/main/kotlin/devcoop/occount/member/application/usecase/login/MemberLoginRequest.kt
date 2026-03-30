9bb1492f2fde4a9e3ab6b1d946fdabead6411a89
Hyunwoo Gu <realization0327@gmail.com>
2026-03-30 17:10:47 +0900

    refactor: 로그인 요청시 파라미터 변수 네이밍을 userEmail -> email로 변경

--- domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/usecase/login/MemberLoginRequest.kt
+++ domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/usecase/login/MemberLoginRequest.kt
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank
 data class MemberLoginRequest(
     @field:NotBlank(message = "이메일은 비어있을 수 없습니다.")
     @field:Email(message = "올바른 이메일 형식이어야 합니다.")
-    val userEmail: String,
+    val email: String,
 
     @field:NotBlank(message = "비밀번호는 비어있을 수 없습니다.")
     val password: String,
