83934b8000dc653812e5236968a31fd51c84c675
Hyunwoo Gu <realization0327@gmail.com>
2026-03-30 20:25:56 +0900

    refactor: 기존 toss sync 서비스 구조 변경 및 퍼사드를 통한 트랜잭션 관리

--- domains/item/item-application/src/main/kotlin/devcoop/occount/item/application/usecase/sync/ApplySoldItemQuantitiesUseCase.kt
+++ domains/item/item-application/src/main/kotlin/devcoop/occount/item/application/usecase/sync/ApplySoldItemQuantitiesUseCase.kt
@@ -2,30 +2,17 @@ package devcoop.occount.item.application.usecase.sync
 
 import devcoop.occount.item.application.output.ItemRepository
 import devcoop.occount.item.application.output.SoldItemPayload
-import devcoop.occount.item.application.output.TossItemPort
-import devcoop.occount.item.domain.item.Item
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ApplySoldItemQuantitiesUseCase(
     private val itemRepository: ItemRepository,
-    private val tossItemPort: TossItemPort,
-    private val syncItemsFromTossUseCase: SyncItemsFromTossUseCase,
 ) {
     @Transactional
-    fun apply() {
-        val soldItems = tossItemPort.getSoldItems()
-        if (soldItems.isEmpty()) {
-            return
-        }
-
+    fun apply(soldItems: List<SoldItemPayload>) {
         val soldItemsByName = soldItems.associateBy(SoldItemPayload::name)
         val soldItemNames = soldItems.map(SoldItemPayload::name)
-
-        if (itemRepository.existsItemByNameIsNotIn(soldItemNames)) {
-            syncItemsFromTossUseCase.sync()
-        }
 
         val updatedItems = itemRepository.findAllByNameIn(soldItemNames).map { item ->
             val soldItem = soldItemsByName[item.getName()]
