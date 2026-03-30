154f64be3cd4c245a8ef2501d8ee33620ab91cdc
Hyunwoo Gu <realization0327@gmail.com>
2026-03-30 09:43:03 +0900

    refactor: 과도한 Transactional 제거

--- domains/payment/payment-application/src/main/kotlin/devcoop/occount/payment/application/usecase/wallet/InitializeWalletUseCase.kt
+++ domains/payment/payment-application/src/main/kotlin/devcoop/occount/payment/application/usecase/wallet/InitializeWalletUseCase.kt
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional
 class InitializeWalletUseCase(
     private val pointWalletRepository: WalletRepository,
 ) {
-    @Transactional
     fun initialize(userId: Long) {
         try {
             pointWalletRepository.save(Wallet(userId))
