432d63f53ff8a2df2c0c96eff3c370bb8c6f17e2
Hyunwoo Gu <realization0327@gmail.com>
2026-03-30 17:31:19 +0900

    refactor: 변경된 엔티티 변수에 맞게 레포지토리 메서드 네이밍도 변경

--- domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/output/UserRepository.kt
+++ domains/member/member-application/src/main/kotlin/devcoop/occount/member/application/output/UserRepository.kt
@@ -5,7 +5,7 @@ import devcoop.occount.member.domain.user.User
 interface UserRepository {
     fun findById(id: Long): User?
     fun findByUserBarcode(userBarcode: String): User?
-    fun findByUserEmail(userEmail: String): User?
-    fun existsByUserEmail(userEmail: String): Boolean
+    fun findByEmail(userEmail: String): User?
+    fun existsByEmail(userEmail: String): Boolean
     fun save(user: User): User
 }
