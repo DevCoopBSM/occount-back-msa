1238e2f9d0de79bece8d26d07744a6584a3bfa8f
Hyunwoo Gu <realization0327@gmail.com>
2026-03-30 15:30:08 +0900

    test: chargeReason 필드에 대한 인풋 추가

--- domains/payment/payment-domain/src/test/kotlin/devcoop/occount/payment/domain/ChargeLogTest.kt
+++ domains/payment/payment-domain/src/test/kotlin/devcoop/occount/payment/domain/ChargeLogTest.kt
@@ -1,6 +1,7 @@
 package devcoop.occount.payment.domain
 
 import devcoop.occount.payment.domain.wallet.ChargeLog
+import devcoop.occount.payment.domain.wallet.ChargeReason
 import devcoop.occount.payment.domain.wallet.PointTransaction
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -15,6 +16,7 @@ class ChargeLogTest {
                 changeAmount = 5000,
                 afterPoint = 5000,
             ),
+            chargeReason = ChargeReason.PURCHASE,
         )
 
         assertEquals(1L, chargeLog.userId)
