b8450c2a10fc067f63a825f4f7de642da2a63c09
Hyunwoo Gu <realization0327@gmail.com>
2026-04-17 22:14:32 +0900

    build: 로컬 개발용 세팅

---
+++ docker/mysql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS occount_member  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS occount_item    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS occount_order   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS occount_payment CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
