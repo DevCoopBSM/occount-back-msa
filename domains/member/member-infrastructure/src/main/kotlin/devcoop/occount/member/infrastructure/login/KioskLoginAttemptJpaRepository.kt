3fc63f9b043ff71793db38f9bd70706eae18f895
hyun731 <gilhyun.gwon0731@gmail.com>
2026-07-01 14:25:53 +0900

    fix: 구현체 수정

--- domains/member/member-infrastructure/src/main/kotlin/devcoop/occount/member/infrastructure/login/KioskLoginAttemptJpaRepository.kt
+++ domains/member/member-infrastructure/src/main/kotlin/devcoop/occount/member/infrastructure/login/KioskLoginAttemptJpaRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import kotlin.time.Instant
+import java.time.Instant
 
 interface KioskLoginAttemptJpaRepository : JpaRepository<KioskLoginAttemptJpaEntity, String> {
     fun findByUserBarcode(userBarcode: String): KioskLoginAttemptJpaEntity?
